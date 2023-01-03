53599e53e082ad3e166b426a4567775e0cc6562b
zzzMunchizzz <jmllerenaespinoza@gmail.com>
2023-01-03 14:03:05 -0500

    Sesion 9 card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
+import { TarjetaComponent } from './cards/tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
